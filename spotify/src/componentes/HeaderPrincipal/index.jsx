1d18c2566614af96ea1cc465a80cb03b57a1543b
Leonardo Batista <LeonardoMTBat@gmail.com>
2023-09-18 23:31:44 -0300

    ajustes

--- spotify/src/componentes/HeaderPrincipal/index.jsx
+++ spotify/src/componentes/HeaderPrincipal/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './HeaderPrincipal.css'
-
+import { Link } from 'react-router-dom';
 const HeaderPrincipal = () => {
     return (
         <nav className='topNavbar'>
             <div className='topNavbar__arrows'>
-                <a href='#'><img src='IMG/arrowLeft.svg'></img></a>
+                <a><img src='IMG/arrowLeft.svg'></img> </a>
                 <a href='#'><img src='IMG/arrowRight.svg'></img></a>
             </div>
             <div className='topNavbar__links'>
-                <a href='#'>FAQ</a>
-                <a href='#'>Sair</a>
+                
+                <Link to='/FAQ'>FAQ</Link>
+                <Link to='/'>Sair</Link>
             </div>
 
         </nav>
